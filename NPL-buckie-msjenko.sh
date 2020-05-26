53064e2cee3c3737c785a8afddd7585ef3ca0cbb
linuxpussy <63728887+linuxpussy@users.noreply.github.com>
2020-05-26 19:37:21 +0200

    NPL's buckie-msjenko sh File

---
+++ NPL-buckie-msjenko.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+Setup=$(sudo mkdir -p CoreDUMP,{CoreCORUS4.0-45, welkinsCoreServer2.0-55, Eclabserver1.0-66, GALA5.9-40,}, Xcore, Gcore, Lxcore, Kcore
+e, NegativeCores, IO && touch CoreDUMP/dumpfile.dump,[allahoakbar, doser.py, pydose, pypoop}
+if [ Setup == "fail" ]; then
+echo "Couldn't launch vasals and tools because Rapport : fail"
+else
+echo "server started at piv.pivpiv.dk"
