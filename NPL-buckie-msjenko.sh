5123b3b4ea1f35b5eaf92fdec02ae5f32bb3bb66
linuxpussy <63728887+linuxpussy@users.noreply.github.com>
2020-05-26 19:56:41 +0200

    Update NPL-buckie-msjenko.sh

--- NPL-buckie-msjenko.sh
+++ NPL-buckie-msjenko.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-Setup=$(sudo mkdir -p CoreDUMP,{CoreCORUS4.0-45, welkinsCoreServer2.0-55, Eclabserver1.0-66, GALA5.9-40,}, Xcore, Gcore, Lxcore, Kcore
-e, NegativeCores, IO && touch CoreDUMP/dumpfile.dump,[allahoakbar, doser.py, pydose, pypoop}
+Setup=$(sudo mkdir -p CoreDUMP && cd CoreDUMP && sudo mkdir Xcore && sudo mkdir fascLINUXcore && sudo mkdir Gcore && sudo mkdir Lxcore && sudo mkdir Kcore && sudo mkdir PowerShlley && sudo mkdir C:\>My name is shelly. I am a psycho. && cd home)
 if [ Setup == "fail" ]; then
 echo "Couldn't launch vasals and tools because Rapport : fail"
 else
